c435669f434d64dc85e2e178c82bc008f79ecc09
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-05-01 10:06:14 -0300

    add migration

--- Data/Migrations/20230501130540_CustomerModel.Designer.cs
+++ Data/Migrations/20230501130540_CustomerModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using registro_hotel_mox_it.Data;
 
@@ -11,9 +12,11 @@ using registro_hotel_mox_it.Data;
 namespace registro_hotel_mox_it.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230501130540_CustomerModel")]
+    partial class CustomerModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
