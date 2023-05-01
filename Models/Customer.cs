0731e48a2fe2111f1ba6917013d1dcc1569c4252
Juani <juan.battelli@gmail.com>
2023-05-01 09:51:20 -0300

    Agregando Modelo Customer

---
+++ Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace registro_hotel_mox_it.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public int? Room { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public DateTime? CheckInDate { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public DateTime? CheckOutDate { get; set; }
+    }
+}
