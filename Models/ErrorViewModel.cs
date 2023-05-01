05e6dd0c5006f5fcbd8483483c3fb64cba33798a
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-05-01 09:29:41 -0300

    setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace registro_hotel_mox_it.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
